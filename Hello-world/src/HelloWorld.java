738d62bb271b23c7895c41c7e6ab7d18d267df64
chimaek <pipiru100@gmail.com>
2019-05-03 13:43:48 +0900

    remote repository add a PushPrint.java

--- Hello-world/src/HelloWorld.java
+++ Hello-world/src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("World's ends dancehall");
 		System.out.println("universe ends dancehall");
-		System.out.println("galaxy3 ends dancehall");
+		System.out.println("galaxy4 ends dancehall");
 	}
 
 }
